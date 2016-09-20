9d0a32766b275d09d24ba8f487a1366e6cdbf554
Anderson <andmendescosta@gmail.com>
2016-09-20 13:42:55 -0300

    Arquivo de Configurações

---
+++ app/src/main/java/br/com/nopredio/util/Configuration.java
@@ -0,0 +1,8 @@
+package br.com.nopredio.util;
+
+/**
+ * Created by u197527 on 15/09/2016.
+ */
+public class Configuration {
+    public static final String URL_APLICTATION = "http://192.168.42.26:8080/nopredio/";
+}
